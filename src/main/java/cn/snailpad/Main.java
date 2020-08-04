50920698cc644967af8173937cc9f18b5621174f
zhouweiming <373518155@qq.com>
2020-08-04 14:52:21 +0800

    在原基础上添加startOfWeek和endOfWeek方法

---
+++ src/main/java/cn/snailpad/Main.java
@@ -0,0 +1,12 @@
+package cn.snailpad;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Jarbon!!!!!!");
+
+        Jarbon jarbon = new Jarbon();
+        System.out.println(jarbon);
+        System.out.println(jarbon.startOfWeek());
+        System.out.println(jarbon.endOfWeek());
+    }
+}
